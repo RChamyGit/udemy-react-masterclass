4614149cee474f7076a24b76a1dae091c7c009de
RChamyGit <39716460+RChamyGit@users.noreply.github.com>
2021-02-08 22:51:56 -0400

    renderização condicional
    se existe login, renderize a home, se ñ , lance componente de Login!

--- src/App.js
+++ src/App.js
@@ -1,49 +1,34 @@
 import React from "react";
 import './styles.css';
-import favourite,{multiplyfav,dividefav} from "./Heading.js"; //qualquer nome q eu escolher aqui vai usar o default do heading
-import Card from "./Card.js";
-import contacts from "./contacts.js";
+import Login from './Login.js'
+// function insertCard(contact){
 
-
-function insertCard(contact){
-
-       return (
-       <Card 
-       id = {contact.id}
-       source = {contact.source}
-       name = {contact.name}  
-       email = {contact.email}
-        />
-       );
-}
+//        return (
+//        <Card 
+//        id = {contact.id}
+//        source = {contact.source}
+//        name = {contact.name}  
+//        email = {contact.email} 
+//        // {/*precisa ser a mesma variavel do parametro...*/}
+//         />
+//        );
+// }
 export default function App() {
+       const isloggedin = false;
+
+       // function checkCondition(){
+       //        if(isloggedin === true) {
+       //               return   <h1>Welcome to The Home Page User</h1>;
+       //        }else{
+       //               return(
+       //               <Login />
+       //               );
+       //        }
+       // } 
+       //mal otimizado
   return (
     <div className = "App">
-           <h2> contacts map</h2>
-           {contacts.map(insertCard)}
-           {/* <h2> fim contacts map</h2>
-      <h1> this is my {favourite}</h1>
-      <h1> ayy multiplicado {multiplyfav()}</h1>
-      <h2>Start editing to see some magic</h2>
-      <h3>Ameno</h3> */}
-
-       {/* <Card 
-              source = "https://i.imgur.com/twfhKLM.jpeg"
-              name = "Gato do React"  
-              email = "rkchamy@hotmail.com"     
-       />
-
-       <Card source = "https://i.imgur.com/twfhKLM.jpeg"
-              name = "latire"  
-              email = "amenoAmenoLatire@Dorime.com"     
-       />
-       <Card 
-            name = {contacts[0].name}  
-            email = "amenoAmenoLatire@Dorime.com"
-            source ={contacts[0].source}     
-     /> */}
-       / comentar aqui faz ele pirar em vez de usar posicao de index manual da pra fazer um loop /
-     </div>
+           {isloggedin?<h1 id="home"> Olá Usuário</h1> : <Login />}     </div>
   
     );
 }
---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Login(){
+    return(
+        <div id="demo">
+        <form>
+               <input class="css-input" type="text" placeholder="Username"/>
+               <input class="css-input" type="text" placeholder="Username"/>
+                                    <input type="submit"/>
+        </form>
+        </div>
+ )
+}
+
+export default Login;
